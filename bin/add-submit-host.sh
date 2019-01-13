517b90746aa9bcaf94ce25e62f85d4cfd97b20e2
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2019-01-14 02:35:21 +0800

    Add more bin and conf

---
+++ bin/add-submit-host.sh
@@ -0,0 +1,1 @@
+qconf -as `hostname`
---
+++ bin/sge-init-conf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+WORK_ROOT=/usr/share/charm-sge-cluster
+CONF_HOSTLIST=${WORK_ROOT}/hostlist.conf
+CONF_SCHEDULER=${WORK_ROOT}/scheduler.conf
+CONF_QUEUE=${WORK_ROOT}/queue.conf
+
+mkdir -p ${WORK_ROOT}
+
+# create host list
+qconf -Msconf ${CONF_SCHEDULER}
+
+# create a host list
+qconf -Ahgrp ${CONF_HOSTLIST}
+# create the queue
+qconf -Aq ${CONF_QUEUE}
+
