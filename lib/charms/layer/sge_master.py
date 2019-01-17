0f1574197d5c7d718d025e2e471f76fac1f23217
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2019-01-18 01:06:29 +0800

    Add add_worker function

--- lib/charms/layer/sge_master.py
+++ lib/charms/layer/sge_master.py
@@ -5,6 +5,7 @@ from subprocess import check_call
 __all__ = ['bootstrap_pre_sge_master', 'get_installed_message']
 
 def bootstrap_pre_sge_master():
+    # prepare basic configuration files and scripts
     dir_bin = '/usr/local/sbin/'
     shutil.copy2('bin/sge-init-conf.sh', dir_bin)
     shutil.copy2('bin/sge-add-work.sh', dir_bin)
@@ -17,6 +18,11 @@ def bootstrap_pre_sge_master():
     shutil.copy2('data/queue.conf', dir_sge)
 
     cmd = os.path.join(dir_bin, 'sge-init-conf.sh')
+    check_call(cmd)
+
+
+def add_worker(worker, slot=1):
+    cmd = ['/usr/local/sbin/sge-init-conf.sh', 'homemade.q',  worker, slot]
     check_call(cmd)
 
 
